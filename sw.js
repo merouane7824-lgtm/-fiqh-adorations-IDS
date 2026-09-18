5bb7f18b043b39a41cffcaa29b2f11beeee4be03
merouane7824-lgtm <merouane7824@gmail.com>
2026-09-18 18:32:20 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
-const CACHE_NAME='fiqh-adorations-v1'; const APP_SHELL=['./','./index.html','./manifest.webmanifest','./sw.js']; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()))); self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))); self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('./index.html'))));});
+const CACHE = "fiqh-adorations-v3";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js", "./icon-512.png", "./icon-1024.png"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match("./index.html"))
+    )
+  );
+});
